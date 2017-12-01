6f3e7f79f2c167b2d0d39d600d2af0e9c1b82113
Vinzenz Aubry <vinzenzaubry@gmail.com>
2017-12-01 17:57:15 +0100

    added: canvasPosition Function

--- js/blueWave.js
+++ js/blueWave.js
@@ -2,15 +2,17 @@
 
 function BlueWave() {
 	this.animationSettings = {
-		xSize: 50,
-		ySize: 50,
+		xSize: 48,
+		ySize: 48,
 		speed: 2,
 		steps: [0.1, 0.1, 0.3, 0.5, 0.5, 1],
 		color: "#305EFF",
 		// speedIn: 0.7,
 		// speedOut: 1.1
-		speedIn: 2,
-		speedOut: 2.8
+		speedIn: 0.5,
+		speedOut: 0.5,
+		canvasTop: 0,
+		canvasLeft: 0
 	}
 
 	this.size = {
@@ -36,6 +38,8 @@ function BlueWave() {
 	document.getElementById('startImage').addEventListener('click', function() {
 		self.runIn();
 	})
+
+	this.setBlueWavePosition(0, 120);
 	
 }
 
@@ -46,8 +50,8 @@ BlueWave.prototype.addCanvas = function () {
 	canv.style.pointerEvents = "none";
 	canv.style.display = 'block';
 	canv.style.position = 'fixed';
-	canv.style.top = 0;
-	canv.style.left = 0;
+	canv.style.top = `${this.animationSettings.canvasTop}px`;
+	canv.style.left = `${this.animationSettings.canvasLeft}px`;
 
 	document.body.appendChild(canv);
 	this.canvas = canv;
@@ -67,7 +71,7 @@ BlueWave.prototype.calculateSettings = function() {
 }
 
 BlueWave.prototype.runIn = function() {
-	let coloumnsString = this.calculatedSettings.coloumns + 14;
+	let coloumnsString = this.calculatedSettings.coloumns + this.animationSettings.steps.length;
 	coloumnsString = coloumnsString.toString();
 	var self = this;
 
@@ -79,29 +83,12 @@ BlueWave.prototype.runIn = function() {
 	// TweenLite.ticker.fps(20);
 }
 
-BlueWave.prototype.updateHandlerRunIn = function(scope, addRectangles) {
-	var distance = Math.round(scope.calculatedSettings.currentDistance);
-	
-	if (distance > scope.calculatedSettings.lastDraw) {
-		// console.log(distance);
-		
-		scope.calculatedSettings.lastDraw = distance;
-		scope.spawnPixels(distance, addRectangles);
-	}	
-}
-
-BlueWave.prototype.completeHandlerRunIn = function(scope) {
-	document.getElementById('startImage').style.display = 'none'
-	scope.runOut();
-}
-
-
 BlueWave.prototype.runOut = function() {
 	//reset values
 	this.calculatedSettings.currentDistance = -2;
 	this.calculatedSettings.lastDraw = -2;
 
-	let coloumnsString = this.calculatedSettings.coloumns + 46;
+	let coloumnsString = this.calculatedSettings.coloumns + this.animationSettings.steps.length;
 	coloumnsString = coloumnsString.toString();
 	var self = this;
 
@@ -112,6 +99,23 @@ BlueWave.prototype.runOut = function() {
 }
 
 
+
+
+
+BlueWave.prototype.updateHandlerRunIn = function(scope, addRectangles) {
+	var distance = Math.round(scope.calculatedSettings.currentDistance);
+	
+	if (distance > scope.calculatedSettings.lastDraw) {
+		// console.log(distance);
+		scope.calculatedSettings.lastDraw = distance;
+		scope.spawnPixels(distance, addRectangles);
+	}	
+}
+
+BlueWave.prototype.completeHandlerRunIn = function(scope) {
+	document.getElementById('startImage').style.display = 'none'
+	scope.runOut();
+}
 
 
 
@@ -156,43 +160,29 @@ BlueWave.prototype.spawnPixels = function(xStep, addRectangles) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-BlueWave.prototype.shuffleArray = function (array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+BlueWave.prototype.setBlueWavePosition = function(x, y) {
+	this.animationSettings.canvasTop = y;
+	this.animationSettings.canvasLeft = x;
+	this.canvas.style.top = `${this.animationSettings.canvasTop}px`;
+	this.canvas.style.left = `${this.animationSettings.canvasLeft}px`;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 BlueWave.prototype.eventListeners = function () {
 	this.resize = this.resize.bind(this);
@@ -214,6 +204,28 @@ BlueWave.prototype.resize = function () {
     this.ctx.scale(this.size.scaleRatio, this.size.scaleRatio);
 
     //everything gets blue if you resize while the animation happens;
+}
+
+
+// =================== SANTAS LITTLE HELPERS =================== //
+
+BlueWave.prototype.shuffleArray = function (array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 
